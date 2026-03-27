a3373ef718b7ad44665b4981674cc34b0364f878
Leah O'Donoghue <leahodonoghue@Leahs-MacBook-Air-3.local>
2026-03-27 13:51:56 +0000

    updates

--- controllers/about.js
+++ controllers/about.js
@@ -1,14 +1,15 @@
 'use strict';
 
 import logger from "../utils/logger.js";
-
+import employeeStore from "../models/employee.js";
 
 const about = {
     createView(request, response) {
       logger.info("About page loading!");
 
       const viewData = {
-        title: "About the Playlist App"
+        title: "About the Playlist App",
+        info: employeeStore.getAppInfo()
       };
 
       response.render('about', viewData); 
--- models/employee.js
+++ models/employee.js
@@ -1,1 +1,17 @@
+'use strict';
 
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
+
+const employeeStore = {
+
+  store: new JsonStore('./models/app-store.json', { info: {} }),
+  collection: 'employee',
+
+  getAppInfo() {
+    return this.store.findAll(this.collection);
+  },
+
+};
+
+export default employeeStore;
