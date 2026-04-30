c9f7b01184e0897521c19d3ec15c0054fbf41246
Leah O'Donoghue <leahodonoghue@Leahs-MacBook-Air-3.local>
2026-05-01 00:50:51 +0100

    finished add function lab

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -21,6 +21,9 @@ addSong(id, song) {
     this.store.addItem(this.collection, id, this.array, song);
 },
 
+addPlaylist(playlist) {
+    this.store.addCollection(this.collection, playlist);
+},
 
 
 };
--- routes.js
+++ routes.js
@@ -16,6 +16,7 @@ router.get('/playlist/:id', playlist.createView);
 
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
 router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 
 export default router;
 
