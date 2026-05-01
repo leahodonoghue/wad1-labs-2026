485b94365249fa0fddfa4f8ea940332abbfed053
Leah O'Donoghue <leahodonoghue@Leahs-MacBook-Air-3.local>
2026-05-01 04:18:48 +0100

    updates

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import routes from './routes.js';
 import { create } from 'express-handlebars';
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 
 const app = express();
@@ -14,6 +15,7 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(cookieParser());
+app.use(fileUpload({useTempFiles: true}));
 
 const handlebars = create({
   extname: '.hbs', 
@@ -32,7 +34,7 @@ formatDate: (date) =>  {
     };
      return `${dateCreated.toLocaleDateString("en-IE", options)}`;
     },
-    
+
 highlightPopular: (rating) => {
    let message = rating >= 4 ? "Popular with listeners!" :  "";
    return message;
