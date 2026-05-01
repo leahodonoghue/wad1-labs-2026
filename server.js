ea57c842b973674752999d4acb755a16643a1a5a
Leah O'Donoghue <leahodonoghue@Leahs-MacBook-Air-3.local>
2026-05-01 01:09:39 +0100

    custom helpers

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict';
 
 import express from 'express';
-import routes from './routes.js'; 
 import logger from "./utils/logger.js";
+import routes from './routes.js'; 
 import { create } from 'express-handlebars';
 import bodyParser from "body-parser";
-
 
 const app = express();
 const port = 3000;
@@ -13,11 +12,17 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false, }));
 
-const handlebars = create({extname: '.hbs'});
+const handlebars = create({
+  extname: '.hbs', 
+    helpers: {
+      uppercase: (inputString) => {
+        return inputString.toUpperCase();
+      },
+    },
+});
 app.engine(".hbs", handlebars.engine);
 app.set("view engine", ".hbs");
 
 app.use("/", routes);
 
-app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
-
+app.listen(port, () => logger.info(`Your app is listening on port ${port}`));
